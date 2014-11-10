53846a12b25c5b1846f5d301f030a8ab430e6840
Adrien BAK <adrien.bak@gmail.com>
2014-11-10 17:13:53 +0900

    checkin

--- pacbackup.py
+++ pacbackup.py
@@ -5,32 +5,49 @@ import argparse
 import pyalpm
 from pycman import config
 
+def pkg_info_str(pkg):
+  return str(pkg.name + "\t" + pkg.version)
 
+class PacBackup:
+  def __init__(self, options):
+    self.handle = config.init_with_config_and_options(options)
+    self.backup_file_path = options.backup_config
+    self.verbosity = options.verbose
 
-def main(args):
-  parser = argparse.ArgumentParser
+  def retrieve_pkg_lists(self):
+    syncpkgs = set()
+    for db in self.handle.get_syncdbs():
+      syncpkgs |= set(p.name for p in db.pkgcache)
 
-  handle = config.init_with_config("/etc/pacman.conf")
-  pyalpm.Handle("/", "/var/lib/pacman")
-  syncpkgs = set()
-  print(handle.get_syncdbs())
-  for db in handle.get_syncdbs():
-    syncpkgs |= set(p.name for p in db.pkgcache)
+    db = self.handle.get_localdb()
 
-  db = handle.get_localdb()
-  pkglist = [x for x in db.pkgcache if x.reason == pyalpm.PKG_REASON_EXPLICIT]
+    self.native_pkg = [x for x in db.pkgcache if x.reason == pyalpm.PKG_REASON_EXPLICIT and x.name in syncpkgs]
+    self.aur_pkg    = [x for x in db.pkgcache if x.reason == pyalpm.PKG_REASON_EXPLICIT and x.name not in syncpkgs]
 
-  native_pkg = [x for x in pkglist if x.name in syncpkgs]
-  aur_pkg    = [x for x in pkglist if x.name not in syncpkgs]
+    if self.verbosity :
+      print("native : ")
+      for pkg in self.native_pkg:
+        print("\t" + pkg_info_str(pkg))
 
-  print("native : ")
-  for pkg in native_pkg:
-    print("\t" + pkg.name)
+      print("AUR : ")
+      for pkg in self.aur_pkg:
+        print("\t" + pkg_info_str(pkg))
 
-  print("AUR : ")
-  for pkg in aur_pkg:
-    print("\t" + pkg.name)
+  def backup_pkg_lists(self):
+
+def main():
+
+  parser = config.make_parser(description='Backs-up the list of pacman-installed packages on the system.', 
+    prog = 'pacbackup')
+
+  group = parser.add_argument_group("Backup options")
+  group.add_argument('--backup-config', metavar='<path>', default='~/.config/pacbackup/pkglist', 
+    help = "specifies the backup file location, default : ~/.config/pacbackup/pkglist")
+  # group.add_argument('--verbose', '-v', action='store_true', help="verbose output")
+
+  backup = PacBackup(parser.parse_args())
+  backup.retrieve_pkg_lists()
+
 
 if __name__ == '__main__':
-  import sys
-  main(sys.argv)
+  main()
