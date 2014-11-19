8627e64249ce7b6362d9172285b9166f93415b67
Adrien BAK <adrien.bak@gmail.com>
2014-11-19 17:50:02 +0900

    Adds setup.py to install things

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name="pacbackup",
+  description="Backup tool for the pacman installed packages",
+  version="1.0.0",
+  author="Adrien Bak",
+  author_email="adrien.bak@gmail.com",
+  scripts=['src/pacbackup.py'],
+  data_files=[('share/pacbackup', ['src/pacrestore.sh'])]
+  )
